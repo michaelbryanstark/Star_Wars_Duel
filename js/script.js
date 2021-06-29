acd044e942a6e2a2e0173580bca8905ba176a53a
Michael Stark <michael.starkb@gmail.com>
2021-06-29 18:07:12 -0500

    create const variables

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const attack;
+const defend;
+const attackcharacter;
+const attackerHP;
+const attackerAP;
+const attackerCAP;
+const defendcharacter;
+const defenderHP;
+const defenderAP;
+const defenderCAP;
+const name;
+const YourCharacter;
+const YourDefender;
+const myChar = "";
+const myDef = "";
